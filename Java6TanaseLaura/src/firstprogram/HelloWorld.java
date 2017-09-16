cb62901b494e49486832875e97594a2f6cbae280
Laurici <Laurici@Fitzoshica>
2017-09-16 17:05:20 +0300

    This is my first program in Java

---
+++ Java6TanaseLaura/src/firstprogram/HelloWorld.java
@@ -0,0 +1,22 @@
+/*
+ * 
+ * 
+ * My first Program written in java
+ */
+package firstprogram;
+
+/**
+ *This is my first class
+ * @author Laurici
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // say hello world
+        System.out.println("Hello World!");
+    }
+    
+}
