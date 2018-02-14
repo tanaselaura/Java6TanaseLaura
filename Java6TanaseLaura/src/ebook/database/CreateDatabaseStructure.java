56449e29cad3baa171db4b42990376197b5c0eba
Laurici <Laurici@Fitzoshica>
2018-02-14 23:57:32 +0200

    xml

--- Java6TanaseLaura/src/ebook/database/CreateDatabaseStructure.java
+++ Java6TanaseLaura/src/ebook/database/CreateDatabaseStructure.java
@@ -30,21 +30,35 @@ public class CreateDatabaseStructure {
             //statement.execute("CREATE TABLE FIRSTTABLE (ID INT PRIMARY KEY, NAME VARCHAR(12))");
             
             //create table BOOK_TYPES
-            statement.execute("create table BOOK_TYPES(TYPE VARCHAR (25)PRIMARY KEY )");
+            statement.execute("create table BOOK_TYPES" +
+                     "(ID INTEGER PRIMARY KEY, "  +
+                     "TYPE VARCHAR (25) )");
+            
+            //create table ROLES
+            statement.execute("create table ROLES(ROLE VARCHAR(10)PRIMARY KEY )");
+            
+             //create table BOOK_PAPER_QUALITIES
+            statement.execute("create table BOOK_PAPER_QUALITIES" +
+                     "(ID INTEGER PRIMARY KEY, "  +
+                     "QUALITY VARCHAR (25) )");
             
             //create table BOOK_GENRES
-            statement.execute("create table BOOK_GENRES(GENRE VARCHAR (25)PRIMARY KEY )");
+            statement.execute("create table BOOK_GENRES" +
+                     "(ID INTEGER PRIMARY KEY, "  +
+                     "GENRE VARCHAR (25) )");
             
             //CREATE TABLE EBOOKS
             statement.execute("CREATE TABLE EBOOKS " +
                     "(ISBN VARCHAR(50) PRIMARY KEY," +
                     " TITLE VARCHAR(50) NOT NULL," +
-                    " TYPE VARCHAR(25), " +
+                    " TYPE INTEGER, " +
                     " PAGES INTEGER, " +
-                    " GENRE VARCHAR(25), " +
+                    " PRET numeric(10,2), " +
+                    " ID_QUALITY INTEGER, " +
+                    " GENRE INTEGER) " );
                     //add foreign key for type and genre
-                    " FOREIGN KEY (TYPE) REFERENCES BOOK_TYPES(TYPE), " +
-                    " FOREIGN KEY (GENRE) REFERENCES BOOK_GENRES(GENRE))");
+                    //" FOREIGN KEY (TYPE) REFERENCES BOOK_TYPES(TYPE), " +
+                    //" FOREIGN KEY (GENRE) REFERENCES BOOK_GENRES(GENRE))");
             
             //CREATE TABLE BOOK_AUTHORS
             statement.execute("CREATE TABLE BOOK_AUTHORS " +
@@ -56,37 +70,40 @@ public class CreateDatabaseStructure {
             statement.execute("CREATE TABLE EBOOKS_AUTHORS " +
                     "(ID INTEGER PRIMARY KEY, "  +
                     " ISBN VARCHAR(50) NOT NULL, " +
-                    " CNP VARCHAR(13), " +
+                    " CNP VARCHAR(13)) " );
                     //add foreign key for ISBN and CNP
-                    " FOREIGN KEY (ISBN) REFERENCES EBOOKS(ISBN), " +
-                    " FOREIGN KEY (CNP) REFERENCES BOOK_AUTHORS(CNP))");
+                   // " FOREIGN KEY (ISBN) REFERENCES EBOOKS(ISBN), " +
+                    //" FOREIGN KEY (CNP) REFERENCES BOOK_AUTHORS(CNP))");
             
             //CREATE TABLE RATINGS
-            statement.execute("CREATE TABLE RATINGS (RATING VARCHAR(5) PRIMARY KEY)");
+            statement.execute("CREATE TABLE RATINGS" +
+                     "(ID INTEGER PRIMARY KEY, "  +
+                     "RATING VARCHAR(5) )");
             
             //CREATE TABLE USERS
             statement.execute("CREATE TABLE USERS " +
                     "(CNP VARCHAR(13) PRIMARY KEY, " +
                     " NAME VARCHAR(50) NOT NULL, " + 
-                    " PASSWORD VARCHAR(50) NOT NULL)");
+                    " PASSWORD VARCHAR(50) NOT NULL, " +
+                    " ROLE VARCHAR(10) )");
             
             //CREATE TABLE EBOOKS_RATING_USERS
             statement.execute("CREATE TABLE EBOOKS_RATING_USERS " +
                     "(ID INTEGER PRIMARY KEY, " +
-                    " RATING VARCHAR(5) NOT NULL, " + 
+                    " RATING INTEGER NOT NULL, " + 
                     " CNP VARCHAR(13) NOT NULL, " + 
-                    " ISBN VARCHAR(50) NOT NULL, " +
+                    " ISBN VARCHAR(50) NOT NULL )" );
                     //add foreing key for raiting CNP and ISBN
-                    " FOREIGN KEY (RATING) REFERENCES RATINGS(RATING), " +
-                    " FOREIGN KEY (CNP) REFERENCES USERS(CNP), " +
-                    " FOREIGN KEY (ISBN) REFERENCES EBOOKS(ISBN))");
+                    //" FOREIGN KEY (RATING) REFERENCES RATINGS(RATING), " +
+                    //" FOREIGN KEY (CNP) REFERENCES USERS(CNP), " +
+                    //" FOREIGN KEY (ISBN) REFERENCES EBOOKS(ISBN))");
             
             //add book types
-            statement.execute("INSERT INTO BOOK_TYPES VALUES ('eBook'),('PDF')");
+            statement.execute("INSERT INTO BOOK_TYPES(id, type) VALUES (1,'eBook'),(2,'PDF'), (3,'Kindle')");
             //add book genres
-            statement.execute("INSERT INTO BOOK_GENRES VALUES ('Science fiction'),('Satire'),('Drama'),('Action and Adventure'),('Romance'),('Mystery'),('Horror'),('Self help'),('Health'),('Guide'),('Travel'),('Children.s'),('Religion & New Age'),('Science'),('History'),('Math'),('Anthology'),('Poetry'),('Encyclopedias'),('Dictionaries'),('Comics'),('Art'),('Cookbooks'),('Diaries'),('Journals'),('Prayer books'),('Series'),('Trilogy'),('Biographies'),('Autobiographies'),('Fantasy')");
+            statement.execute("INSERT INTO BOOK_GENRES(id, genre) VALUES (1,'Science fiction'),(2,'Satire'),(3,'Drama'),(4,'Action and Adventure'),(5,'Romance'),(6,'Mystery'),(7,'Horror'),(8,'Self help'),(9,'Health'),(10,'Guide'),(11,'Travel'),(12,'Children.s'),(13,'Religion & New Age'),(14,'Science'),(15,'History'),(16,'Math'),(17,'Anthology'),(18,'Poetry'),(19,'Encyclopedias'),(20,'Dictionaries'),(21,'Comics'),(22, 'Art'),(23, 'Cookbooks'),(24, 'Diaries'),(25,'Journals'),(26,'Prayer books'),(27,'Series'),(28,'Trilogy'),(29,'Biographies'),(30,'Autobiographies'),(31,'Fantasy')");
             //add stars for ratings
-            statement.execute("INSERT INTO RATINGS VALUES ('*'), ('**'), ('***'), ('****'), ('*****')");
+            statement.execute("INSERT INTO RATINGS(id, RATING) VALUES (1,'*'), (2,'**'), (3,'***'), (4,'****'), (5,'*****')");
         } catch (SQLException e) {
             System.out.println(e);
         }
---
+++ Java6TanaseLaura/src/xml/ValidateUser.java
@@ -0,0 +1,102 @@
+/*
+ * package for xml user-password validation
+ */
+package xml;
+
+import java.io.File;
+import java.io.IOException;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.DOMException;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
+import java.util.Scanner;
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.SAXParseException;
+
+/**Class ValidateUsers - checks if name and passwords pairs are valid.
+ *@author Laurici
+ */
+public class ValidateUser {
+
+    /**
+     * main method running the validation
+     * @param args
+     */
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        String inputUsername;
+        String inputPassword;
+        Boolean found=false;
+        
+        //Ask user to input username
+        System.out.print("Insert username:");
+        inputUsername = scanner.next();
+        //Ask user to input password
+        System.out.print("Insert password:");
+        inputPassword = scanner.next();
+        
+        try {
+         //Add xml file's path
+         File inputFile = new File("C:\\Users\\Laurici\\Documents\\NetBeansProjects\\clone\\Java6TanaseLaura\\Java6TanaseLaura\\src\\xml\\Users.xml");
+         //Create a factory instance to obtain a parser
+         DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+         dbFactory.setValidating(true);
+         //New document builder parser
+         DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+         dBuilder.setErrorHandler(new ErrorHandler() { 
+             @Override
+             public void warning(SAXParseException saxpe) throws SAXException {
+                  saxpe.printStackTrace();
+             }
+
+             @Override
+             public void error(SAXParseException saxpe) throws SAXException {
+                 saxpe.printStackTrace();
+             }
+
+             @Override
+             public void fatalError(SAXParseException saxpe) throws SAXException {
+                 saxpe.printStackTrace();
+             }
+         });
+         
+         //Parse the xml
+         Document doc = dBuilder.parse(inputFile);
+         //Put nodes in a tree
+         doc.getDocumentElement().normalize();
+         //Returns a NodeList of all the Elements 
+         NodeList nList = doc.getElementsByTagName("user");
+         //get number of elements
+         int length = nList.getLength();
+         
+         for (int temp = 0; temp < length; temp++) {
+            Node nNode = nList.item(temp);
+            //Check if node is an element
+            if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+               Element eElement = (Element) nNode;
+               //Put all name values in an Array of Strings
+               String[] names = (eElement.getElementsByTagName("name").item(0).getTextContent()).split(" ");
+               //Put all passwords values in an Array of Strings
+               String[] pass = (eElement.getElementsByTagName("password").item(0).getTextContent()).split(" ");
+                
+                for (int i=0; i<names.length; i++) {
+                    // match userinput with names and passwords in the arrays
+                    if (names[i].equals(inputUsername)&& pass[i].equals(inputPassword)){
+                        System.out.println("Login succesful!");
+                        found=true;
+                    }                    
+                }    
+            }
+        }
+        if (!found){System.out.println("Wrong username or password");}
+        } catch (ParserConfigurationException | SAXException | IOException | DOMException e) {
+         e.printStackTrace();
+        }
+    }
+}
